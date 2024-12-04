80528d22b2b2441ac9c4fac91939d1a1090808ec
Ed Corcoran <ecorcoran@wyzant.com>
2024-12-03 22:02:19 -0600

    Wrong code I'll have to ditch

--- day2.py
+++ day2.py
@@ -1,6 +1,6 @@
 # Part One
-# fhand = open('day2sample.txt')
-fhand = open('day2.txt')
+fhand = open('day2sample.txt')
+# fhand = open('day2.txt')
 
 reports = list()
 
@@ -47,27 +47,38 @@ for line in fhand:
    levels = [int(x) for x in levels]
    reports.append(levels)
 
-monotonic = bool()
-gradual = bool()
 SafeCount = 0
 
 for levels in reports:
-    if levels == sorted(levels):
-        monotonic = True
-    elif levels == sorted(levels, reverse=True):
-        monotonic = True
-    else:
-        monotonic = False 
-    gradual = True
-    for i in range(len(levels)):
-        if i == 0: continue
-        if abs(levels[i] - levels[i-1]) < 1:
-            gradual = False
-        if abs(levels[i] - levels[i-1]) > 3:
-            gradual = False
-    if monotonic and gradual:
+    ErrorCount = 0
+    PrevLevel = 0
+    CurrLevel = 0
+    IncreasingFlag = False
+    DecreasingFlag = False
+    for index, level in enumerate(levels):
+            if index == 0:
+                CurrLevel = level
+            elif index == 1:
+                PrevLevel = CurrLevel
+                CurrLevel = level
+                if CurrLevel == PrevLevel: ErrorCount += 1
+                if CurrLevel > PrevLevel: IncreasingFlag = True
+                if CurrLevel < PrevLevel: DecreasingFlag = True
+                if abs(CurrLevel - PrevLevel) > 3: ErrorCount += 1
+            else:
+                PrevLevel = CurrLevel
+                CurrLevel = level
+                if IncreasingFlag and CurrLevel <= PrevLevel: 
+                    ErrorCount += 1
+                    continue
+                if DecreasingFlag and CurrLevel >= PrevLevel: 
+                    ErrorCount += 1
+                    continue
+                if abs(CurrLevel - PrevLevel) > 3: 
+                    ErrorCount += 1
+                    continue 
+    if ErrorCount <= 1:
         SafeCount += 1
-    # print(levels, "Monotonic:", monotonic, "Gradual", gradual)
-    # input()
+       
 
-print(SafeCount, "Safe Reports")
+print(SafeCount, "Safe Reports with Dampener")
