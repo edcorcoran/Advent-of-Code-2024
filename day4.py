fd345aed40e8c7cfbf619565ab6cb898448d982a
Ed Corcoran <ecorcoran@wyzant.com>
2024-12-05 17:07:21 -0600

    Solution for Day 4 part 2

--- day4.py
+++ day4.py
@@ -64,4 +64,39 @@ for i, row in enumerate(puzzle):
             if i < row_max and j > 0: wordcount += WordSearch(i+1,j-1,1,'downleft')
             if i < row_max and j < col_max: wordcount += WordSearch(i+1,j+1,1,'downright')
 
-print('Found Word Count', wordcount)
+print('Part One Word Count', wordcount)
+
+# Part Two
+
+# fhand = open('day4sample.txt')
+fhand = open('day4.txt')
+
+puzzle = list()
+
+for line in fhand:
+   puzzle.append(list(line.strip()))
+
+row_max = len(puzzle)-1 # minus one to handle start at zero
+col_max = len(puzzle[0])-1
+wordcount = 0
+
+for i, row in enumerate(puzzle):
+    for j, cell in enumerate(row):
+        if cell == 'A': 
+            mascount = 0
+            if i > 0 and j > 0 and i < row_max and j < col_max: #not too close to an edge 
+                upleft = puzzle[i-1][j-1]
+                downright = puzzle[i+1][j+1]
+                if upleft == 'S' and downright == 'M': mascount += 1
+                if upleft == 'M' and downright == 'S': mascount += 1
+            
+            if i > 0 and j < col_max and i < row_max and j > 0: #not too close to an edge 
+                upright = puzzle[i-1][j+1]
+                downleft = puzzle[i+1][j-1]
+                if upright == 'S' and downleft == 'M': mascount += 1
+                if upright == 'M' and downleft == 'S': mascount += 1
+            
+            if mascount == 2: wordcount += 1
+            
+
+print('Part Two Word Count', wordcount)
